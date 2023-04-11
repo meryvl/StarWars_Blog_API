98351064a14172994286ba0fdc82bf4a680f171b
meryvl <meryvalerolaguna@gmail.com>
2023-04-11 10:34:53 +0000

    new commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User , People , Planet ,Favoritos
 #from models import Person
 
 app = Flask(__name__)
@@ -26,24 +26,74 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+
+
+
 
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def getUsers():
+    try:
+        users = User.query.all()
+        toReturn = [users.serialize() for users in users]
+        return jsonify(toReturn), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    except Exception:
+        return jsonify({"msg": "Ha ocurrido un error"}) , 500
+    
+@app.route('/people', methods=['GET'])
+def getpeoples():
+    try:
+        peoples = People.query.all()
+        toReturnPeoples = [peoples.serialize() for peoples in peoples]
+        return jsonify(toReturnPeoples), 200
 
-    return jsonify(response_body), 200
+    except Exception:
+        return jsonify({"msg": "Ha ocurrido un error"}) , 500
+    
+
+
+@app.route('/people/<int:position>', methods=['GET'])
+def getPeopleId(position):
+    try:
+       
+        peopleId =  People.query.filter_by(People[position])
+        
+        return jsonify(people.serialize()), 200
+
+    except Exception:
+        return jsonify({"msg": "Ha ocurrido un error"}) , 500
+    
+       
+
+@app.route('/Planet', methods=['GET'])
+def getPlanets():
+    try:
+        planets = Planet.query.all()
+        toReturnPlanets = [planets.serialize() for planets in planets]
+        return jsonify(toReturnPlanets), 200
+
+    except Exception:
+        return jsonify({"msg": "Ha ocurrido un error"}) , 500
+    
+       
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,13 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
+
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+
     def __repr__(self):
         return '<User %r>' % self.username
+    
 
     def serialize(self):
         return {
@@ -17,3 +21,112 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Favoritos(db.Model):
+    __tablename__ = 'favoritos'
+   
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    usuario = db.relationship(User)
+
+
+    def __repr__(self):
+        return '<Favoritos %r>' % self.favoritosname
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario id": self.usuario_id,
+            "usuario" : self.usuario
+        }
+
+
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+  
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250),nullable=False)
+    descripcion = db.Column(db.String(250))
+    population = db.Column(db.String(250))
+    Diameter = db.Column(db.String(250))
+    atmósfera = db.Column(db.String(250))
+    
+    def __repr__(self):
+        return '<Planet %r>' % self.plnatname
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.descripcion,
+            "population": self.population,
+            "dimension" : self.Diameter,
+            "atmosfera" : self.atmosfera,
+    
+        }
+
+
+class Vehicles(db.Model): 
+     __tablename__ = 'vehicles'
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(250),nullable=False)
+     Model = db.Column(db.String(250))
+     cargo_capacity= db.Column(db.String(250))
+     consumables= db.Column(db.String(250))
+     favorito_id = db.Column(db.Integer, db.ForeignKey('favoritos.id'))
+     favoritos = db.relationship(Favoritos)
+     
+     
+     def __repr__(self):
+        return '<Vehicles %r>' % self.planetname
+
+     def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "Model": self.Model,
+            "capacity": self.cargo_capacity,
+            "comsumable" : self.consumables,
+            "favorito id" : self.favorito_id,
+            "favorito" : self.favoritos
+    
+        }
+
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    descripcion = db.Column(db.String(250))
+    height = db.Column(db.String(250))
+    year= db.Column(db.String(250))
+   
+    
+
+    def __repr__(self):
+        return '<People %r>' % self.peoplename
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "descripcion ": self.descripcion ,
+            "height": self.height,
+            "year" : self.year,
+            
+           
+    
+        }
+
+
+
+
+    
+    
+    
+    
+    
